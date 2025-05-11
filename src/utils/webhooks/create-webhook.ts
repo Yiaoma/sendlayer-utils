dc9f14a31e237f66126be4dc4f96cd5de06b7d35
Artiom Kalugin <76454716+yiaoma@users.noreply.github.com>
2025-05-11 12:25:20 +0100

    feat: create webhook util (#3)
    Co-authored-by: Artiom <artiom@webstudioguys.com>

---
+++ src/utils/webhooks/create-webhook.ts
@@ -0,0 +1,12 @@
+import { sendLayerRequest } from "../../lib/send-layer-request.js";
+import { WebhookResponse, WebhookPayload } from "../../types/webhooks.js";
+
+export async function createWebhooks(
+  payload: WebhookPayload
+): Promise<WebhookResponse[]> {
+  return sendLayerRequest<WebhookResponse[]>({
+    method: "POST",
+    endpoint: "/webhooks",
+    body: payload,
+  });
+}
